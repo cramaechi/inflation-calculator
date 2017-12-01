a5eb49a8dd589d37f7383e035581d706538f2f62
Chibuikem Amaechi <cramaechi@gmail.com>
2017-11-30 19:46:45 -0600

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	g++ main.cpp -o inflation_calculator
+
+clean:
+	rm inflation_calculator
---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double computeInflationRate(double pastPrice, double currentPrice);
+//Precondition: pastPrice and currentPrice are non-zero, non-negative integers.
+//Returns the rate of inflation, given the items past price and current price.
+
+void clearScreen();
+//Clears the console Screen.
+
+
+int main(int argc, char* argv[])
+{
+	char ans;
+	do
+	{
+		double pPrice, cPrice;
+
+		cout<<"Enter the price of a random item from one year ago and today: ";
+		cin>>pPrice;
+		cin>>cPrice;
+		cout<<endl;
+		cout<<"The inflation rate for the past year is ";
+		cout.setf(ios::showpoint);
+		cout.setf(ios::fixed);
+		cout.precision(1);
+		cout<<computeInflationRate(pPrice, cPrice)<<"%\n\n";
+		cout<<"Start over? (y/n): ";
+		cin>>ans;
+		
+		if(ans == 'Y' || ans == 'y')
+			clearScreen();
+	}while(ans == 'Y' || ans == 'y');
+
+	return 0;
+}
+
+double computeInflationRate(double pPrice, double cPrice)
+{
+	double iR = ((cPrice - pPrice)/pPrice);
+
+	return iR;
+}
+
+void clearScreen()
+{
+	cout<<"\n\n";
+}
